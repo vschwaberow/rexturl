72ecbddd927fe2025d4ca5a447eeee3a03a78760
Volker Schwaberow <volker@schwaberow.de>
2022-09-17 08:42:25 +0200

    Added sort and unique functionality

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,8 @@ struct Config {
     path: bool,
     query: bool,
     fragment: bool,
+    vec_sort: bool,
+    vec_unique: bool,
     all: bool,
 }
 
@@ -81,6 +83,8 @@ fn main() {
         path: false,
         query: false,
         fragment: false,
+        vec_unique: false,
+        vec_sort: false,
         all: false,
     };
 
@@ -100,11 +104,15 @@ fn main() {
             "-P" | "--path" => { c.path = true; },
             "-q" | "--query" => { c.query = true; },
             "-f" | "--fragment" => { c.fragment = true; },
+            "-S" | "--sort" => { c.vec_sort = true; },
+            "-U" | "--unique" => { c.vec_unique = true; },
             "-a" | "--all" => { c.all = true; },
             "-h" | "--help" => { print_help(); },
             _ => (),
         }
     }
+
+    let res_vec: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
 
     for line in stdin.lock().lines() {
         let line = line.unwrap();
@@ -116,43 +124,44 @@ fn main() {
         let url = url.unwrap();
 
         let c = config_sptr.borrow();
+        let mut e = RefCell::borrow_mut(&res_vec);
 
         if c.scheme {
             let scheme = url.scheme();
-            println!("{}", scheme);
+            e.push(format!("{}", scheme));
             continue; 
         } else if c.username {
             let username = url.username();
             match username {
                 "" => (),
-                _ => println!("{}", username),
+                _ =>  { e.push(format!("{}", username)); },
             }
         } else if c.host {
             let host = url.host();
             match host {
-                Some(host) => println!("{}", host),
-                None => println!("No hostname"),
+                Some(host) => { e.push(format!("{}", host)); },
+                None => continue,
             }
         } else if c.port {
             let port = url.port();
             match port {
-                Some(port) => println!("{}", port),
-                None => println!("No port"),
+                Some(port) => { e.push(format!("{}", port)); },
+                None => continue,
             }
         } else if c.path {
             let path = url.path();
-            println!("{}", path);
+            e.push(format!("{}", path));
         } else if c.query { 
             let query = url.query();
             match query {
-                Some(query) => println!("{}", query),
-                None => println!("No query"),
+                Some(query) => { e.push(format!("{}", query)); },
+                None => continue,
             }
         } else if c.fragment {
             let frag = url.fragment();
             match frag {
-                Some(frag) => println!("{}", frag),
-                None => println!("No fragment"),
+                Some(frag) => { e.push(format!("{}", frag)); },
+                None => continue,
             }
         } else if c.all {
             println!("{}", url);
@@ -161,6 +170,18 @@ fn main() {
             std::process::exit(1);
             
         }
+    }
+
+    if config_sptr.borrow().vec_sort {
+        res_vec.borrow_mut().sort();
+    }
+    
+    if config_sptr.borrow().vec_unique {
+        res_vec.borrow_mut().dedup();
+    }
+
+    for e in res_vec.borrow().iter() {
+        println!("{}", e);
     }
 
 }
